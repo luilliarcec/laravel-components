0f8ef8d0208a3b28dc62a04fca77ab9abf938e54
Luis Arce <luilliarcec@gmail.com>
2022-06-25 20:15:33 -0500

    WIP modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -2,33 +2,42 @@
     'darkMode' => false,
 ])
 
-<div x-data="{open: false, data: {}}" x-show="open"
-     role="dialog" aria-modal="true"
-     x-transition:enter="ease-out duration-300"
-     x-transition:enter-start="opacity-0"
-     x-transition:enter-end="opacity-100"
-     x-transition:leave="ease-in duration-200"
-     x-transition:leave-start="opacity-100"
-     x-transition:leave-end="opacity-0"
-     style="display: none;"
-    {{ $attributes->class('fixed z-50 inset-0 overflow-y-auto') }}
+<div
+    x-data="{open: false, data: {}}" x-show="open"
+    role="dialog" aria-modal="true"
+    x-transition:enter="ease-out duration-300"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="ease-in duration-200"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0"
+    style="display: none;"
+    {{ $attributes->class('relative z-10') }}
 >
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 bg-black/50 transition-opacity" aria-hidden="true"></div>
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+    <div class="fixed inset-0 bg-black/50 transition-opacity"></div>
+
+    <div class="fixed z-10 inset-0 overflow-y-auto">
         <div
-            @class([
-                'inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full',
-                'dark:bg-gray-800' => $darkMode
-            ])
             x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0"
         >
-            {{ $slot }}
+            <div
+                @class([
+                    'relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6',
+                    'dark:bg-gray-800' => $darkMode,
+                ])
+            >
+                {{ $slot }}
+
+                <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                    {{ $actions }}
+                </div>
+            </div>
         </div>
     </div>
 </div>
